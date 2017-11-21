c4f78cf050c40e7496f45e8202f908c1332ba936
Waghmare <cwr.Sachin.Waghmare@int.asurion.com>
2017-11-21 04:58:19 -0600

    mod

--- StartVM.ps1
+++ StartVM.ps1
@@ -5,3 +5,4 @@ $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
      
 get-azurermvm  -name $vmname -resourcegroupname $RGname | Start-Azurermvm -name  $vmname 
+Write-host "Hello World"
