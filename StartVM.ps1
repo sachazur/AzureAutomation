0aab435b515ab5dbce6b6463b9d0d3f3f459ac16
sachazur <31879385+sachazur@users.noreply.github.com>
2017-11-21 04:33:00 -0600

    Created StartVM.ps1 in AzureAutomation.

---
+++ StartVM.ps1
@@ -0,0 +1,7 @@
+Param($vmname,
+      $RGname
+     )
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+     
+get-azurermvm  -name $vmname -resourcegroupname $RGname | Start-Azurermvm -name  $vmname 
